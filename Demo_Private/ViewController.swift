b43ce9856ee8618be925ad848d90d82fbe0ce3cd
Michael Lin <miclin@berkeley.edu>
2020-08-26 18:34:53 -0700

    Initial Commit

---
+++ Demo_Private/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo_Private
+//
+//  Created by Michael Lin on 8/26/20.
+//  Copyright © 2020 Michael Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
